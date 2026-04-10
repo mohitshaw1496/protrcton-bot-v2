bb133b181617510d8bf529724db500478abbbfe8
mohitshaw1496 <darkdictator691@gmail.com>
2026-04-10 23:41:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import sqlite3, hashlib, time
 API_TOKEN = "8498039053:AAFF0bLEb08q10IX8A90F5DZQ_PH7kQNfdo"
 BOT_USERNAME = "Vertex_Protrctor_Robot"
 FORCE_CHANNEL = "@Z_Vertex_01"
-ADMIN_ID = 7947256130
+ADMIN_ID = "7947256130"
 BASE_URL = "https://protrcton-bot-v2.onrender.com"
 
 bot = telebot.TeleBot(API_TOKEN)
